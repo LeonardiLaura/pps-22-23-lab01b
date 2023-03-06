0efd7d98fe2b7dc14f558110c7e7eaa8a0073b25
LeonardiLaura <73897108+LeonardiLaura@users.noreply.github.com>
2023-03-06 10:48:28 +0100

    test base

---
+++ src/e1/LogicTest.java
@@ -0,0 +1,41 @@
+package e1;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class LogicTest {
+    private static final int SIZE = 5;
+    private static final int KNIGHT_ROW = 1;
+    private static final int KNIGHT_COLUMN = 1;
+    private static final int KNIGHT_MOVEMENT_ROW = 2;
+    private static final int KNIGHT_MOVEMENT_COLUMN = 3;
+    private static final int PAWN_ROW = 4;
+    private static final int PAWN_COLUMN = 2;
+    private Logics logic ;
+
+    @BeforeEach
+    void beforeEach(){
+        this.logic = new LogicsImpl(SIZE, KNIGHT_ROW, KNIGHT_COLUMN, PAWN_ROW, PAWN_COLUMN);
+    }
+
+    @Test
+    void testKnight(){
+        Assertions.assertTrue(this.logic.hasKnight(KNIGHT_ROW, KNIGHT_COLUMN));
+    }
+
+    @Test
+    void testPawn(){
+        Assertions.assertTrue(this.logic.hasPawn(PAWN_ROW, PAWN_COLUMN));
+    }
+
+    @Test
+    void testHit(){
+        Assertions.assertFalse(this.logic.hit(KNIGHT_MOVEMENT_ROW, KNIGHT_MOVEMENT_COLUMN));
+        Assertions.assertTrue(this.logic.hit(PAWN_ROW, PAWN_COLUMN));
+    }
+
+
+    
+
+}
--- src/e1/LogicsImpl.java
+++ src/e1/LogicsImpl.java
@@ -14,7 +14,13 @@ public class LogicsImpl implements Logics {
         this.pawn = this.randomEmptyPosition();
         this.knight = this.randomEmptyPosition();	
     }
-    
+
+	public LogicsImpl(int size, int knightRow, int knightColumn, int pawnRow, int pawnColumn) {
+		this.size = size;
+		this.pawn = new Pair<>(pawnRow, pawnColumn);
+		this.knight =  new Pair<>(knightRow, knightColumn);
+	}
+
 	private final Pair<Integer,Integer> randomEmptyPosition(){
     	Pair<Integer,Integer> pos = new Pair<>(this.random.nextInt(size),this.random.nextInt(size));
     	// the recursive call below prevents clash with an existing pawn
