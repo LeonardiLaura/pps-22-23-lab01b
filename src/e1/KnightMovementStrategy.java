32fdfc102df0118ced01d539fb4de8b57b51e4de
LeonardiLaura <73897108+LeonardiLaura@users.noreply.github.com>
2023-03-08 16:12:56 +0100

    refactor

--- src/e1/KnightMovementStrategy.java
+++ src/e1/KnightMovementStrategy.java
@@ -5,9 +5,6 @@ public class KnightMovementStrategy implements PieceMovementStrategy {
     public boolean checkMovement(int previousRow, int previousColumn, int newRow, int newColumn) {
         int x = newRow - previousRow;
         int y = newColumn - previousColumn;
-        if (x!=0 && y!=0 && Math.abs(x)+Math.abs(y)==3) {
-            return true;
-        }
-        return false;
+        return (x!=0 && y!=0 && Math.abs(x)+Math.abs(y)==3);
     }
 }
